fbd3e974d277914b088d37f87ac8b26345bfeaea
yuzu <yuzukaw@yahoo.co.jp>
2017-03-27 12:12:28 +0900

    タイトルとルーティング追加

--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get  'static_pages/about'
+
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 match ':controller(/:action(/:id))',via:[:get , :post , :patch]
